8c74fea4a9fde9b114e0c27ed13e7b8a3a37fa15
Ahmed Khan <ahmed.khan9327@gmail.com>
2026-09-15 11:25:33 -0700

    Add: products-of-array-discluding-self - submission-1

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-1.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        vector<int> outputs(nums.size(), 1);
+
+        int prefix = 1;
+
+        for (int i = 0; i < nums.size(); i++) {
+            outputs[i] = prefix;
+            prefix *= nums[i];
+        }
+
+        int postfix = 1;
+
+        for (int i = nums.size() - 1; i >= 0; i--) {
+            outputs[i] *= postfix;
+            postfix *= nums[i];
+        }
+
+        return outputs;
+    }
+};
